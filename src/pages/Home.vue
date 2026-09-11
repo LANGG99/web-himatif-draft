17676e8ad14aa98cd60b8ae202fbe2e48d300522
itsriojg <riog2911@gmail.com>
2026-09-11 01:28:53 +0000

    fix: sinkronisasi Mintif dengan layout terbaru (overlay + FAB + route maintenance)

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,6 +4,8 @@ import Footer from "../component/Footer.vue";
 import { ref, onMounted, onUnmounted } from "vue";
 
 import FaqAccordion from "../component/FaqAccordion.vue";
+import MintifFab from "../component/MintifFab.vue";
+import { tibaDiHalaman } from "../composables/useMintifReveal.js";
 import { useFaq } from "../composables/constants/UseFaq.js";
 
 const { faqList } = useFaq();
@@ -20,6 +22,7 @@ const scrollToTop = () => {
 
 onMounted(() => {
   window.addEventListener("scroll", handleScroll);
+  tibaDiHalaman(); // susutkan circle overlay kalau pulang dari chatbot Flask
 });
 onUnmounted(() => {
   window.removeEventListener("scroll", handleScroll);
@@ -120,4 +123,7 @@ onUnmounted(() => {
 
   <!-- Footer -->
   <Footer />
+
+  <!-- FAB Mintif: khusus halaman utama -->
+  <MintifFab />
 </template>
